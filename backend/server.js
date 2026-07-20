f645062367f9597b9add08d1d93dbb8ea769edc1
Ali Mujahid <mindcracker54321@gmail.com>
2026-07-21 01:08:10 +0500

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import app from "./src/app.js";
 import envConfig from "./src/config/config.js";
 import database from "./src/config/database.js";
+import dns from "node:dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 database();
 
