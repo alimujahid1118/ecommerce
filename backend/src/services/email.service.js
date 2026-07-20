c493dc93f0696c3897b39f7130fed4d0ccae80fe
Ali Mujahid <mindcracker54321@gmail.com>
2026-07-21 00:54:27 +0500

    Changes

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -2,28 +2,36 @@ import envConfig from "../config/config.js";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-        type: 'OAuth2',
+        type: "OAuth2",
         user: envConfig.GOOGLE_USER,
         clientId: envConfig.CLIENT_ID,
         clientSecret: envConfig.CLIENT_SECRET,
-        refreshToken: envConfig.GOOGLE_REFRESH_TOKEN
+        refreshToken: envConfig.GOOGLE_REFRESH_TOKEN,
+    },
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("SMTP Ready");
     }
-})
+});
 
 async function sendEmail(to, subject, text, html) {
-    try {
-        const info = await transporter.sendMail({
-            from: `E Shop <${envConfig.GOOGLE_USER}>`,
-            to,
-            subject,
-            text,
-            html
-        })
-    } catch (error) {
-        console.log(error)
-    }
+    const info = await transporter.sendMail({
+        from: `E Shop <${envConfig.GOOGLE_USER}>`,
+        to,
+        subject,
+        text,
+        html,
+    });
+
+    console.log("Email sent:", info.messageId);
+
+    return info;
 }
 
 export default sendEmail;
