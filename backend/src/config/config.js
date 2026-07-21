f837b19d3a5bc8add6c84bdb473dfd3a7ae971ce
Ali Mujahid <mindcracker54321@gmail.com>
2026-07-21 10:14:32 +0500

    Credentials

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -14,32 +14,19 @@ if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI not found in environment variables.");
 }
 
-if (!process.env.CLIENT_SECRET) {
-    throw new Error("CLIENT_SECRET not found in environment variables.");
+if (!process.env.BREVO_LOGIN) {
+    throw new Error("BREVO_LOGIN not found in environment variables.");
 }
 
-if (!process.env.CLIENT_ID) {
-    throw new Error("CLIENT_ID not found in environment variables.");
-}
-
-if (!process.env.GOOGLE_REFRESH_TOKEN) {
-    throw new Error("GOOGLE_REFRESH_TOKEN not found in environment variables.");
-}
-
-if (!process.env.GOOGLE_USER) {
-    throw new Error("GOOGLE_USER not found in environment variables.");
+if (!process.env.BREVO_API_KEY) {
+    throw new Error("BREVO_API_KEY not found in environment variables.");
 }
 
 const envConfig = {
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     PORT: process.env.PORT,
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET,
-    GOOGLE_USER: process.env.GOOGLE_USER,
-    GOOGLE_REFRESH_TOKEN: process.env.GOOGLE_REFRESH_TOKEN,
     BREVO_LOGIN: process.env.BREVO_LOGIN,
-    BREVO_SMTP_KEY: process.env.BREVO_SMTP_KEY,
     BREVO_API_KEY: process.env.BREVO_API_KEY
 }
 
