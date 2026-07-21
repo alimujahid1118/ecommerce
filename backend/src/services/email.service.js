7b831a4faa1a40a5010589f3d6352556cd08db45
Ali Mujahid <mindcracker54321@gmail.com>
2026-07-21 09:11:20 +0500

    Fix

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -5,6 +5,10 @@ const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
+
+    logger: true,
+    debug: true,
+
     auth: {
         user: process.env.BREVO_LOGIN,
         pass: process.env.BREVO_SMTP_KEY,
