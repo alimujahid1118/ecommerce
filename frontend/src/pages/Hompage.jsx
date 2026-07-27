cac4fb4f70a8f86bd2c12e5625b4ed16e11d1185
Ali Mujahid <mindcracker54321@gmail.com>
2026-07-27 23:18:45 +0500

    HomePage improvements

--- frontend/src/pages/Hompage.jsx
+++ frontend/src/pages/Hompage.jsx
@@ -45,45 +45,46 @@ export default function Homepage() {
             </div>
 
             {/* Sponsored - Banner Image */}
-            <div className="flex flex-col items-center px-10">
-                <img src={discountBanner} alt="discount-banner" className="w-auto max-w-[1440px] mx-6 rounded-lg"/>
+            <div className="flex flex-col w-full items-center px-10">
+                <img src={discountBanner} alt="discount-banner" className="w-full md:max-w-[1440px] mx-6 rounded-lg"/>
             </div>
 
             {/* Store features */}
-
-            <div className="grid grid-cols-2 px-6 py-6 items-center gap-4">
-                <div className="flex flex-col border-[1px] rounded-lg border-[#244e65] items-center justify-center text-center h-40 px-6 shadow-lg">
-                    <i className="fi fi-rr-shipping-fast text-[#104185] text-3xl"></i>
-                    <p className="text-[#132A36] font-semibold mb-2">Express Delivery</p>
-                    <p className="text-[#104185] text-xs">All over Country</p>
-                </div>
-                <div className="flex flex-col border-[1px] rounded-lg border-[#244e65] items-center justify-center text-center h-40 px-6 shadow-lg">
-                    <i className="fi fi-rr-feedback-alt text-[#104185] text-3xl"></i>
-                    <p className="text-[#132A36] font-semibold mb-2">Positive Feedback</p>
-                    <p className="text-[#104185] text-xs">99% Customer satisfaction rate</p>
-                </div>
-                <div className="flex flex-col border-[1px] rounded-lg border-[#244e65] items-center justify-center text-center h-40 px-6 shadow-lg">
-                    <i className="fi fi-rr-restock text-[#104185] text-3xl"></i>
-                    <p className="text-[#132A36] font-semibold mb-2">Easy Return and refunds</p>
-                    <p className="text-[#104185] text-xs">Amazon delivery charges apply</p>
-                </div>
-                <div className="flex flex-col border-[1px] rounded-lg border-[#244e65] items-center justify-center text-center h-40 px-6 shadow-lg">
-                    <i className="fi fi-rr-chart-mixed-up-circle-dollar text-[#104185] text-3xl"></i>
-                    <p className="text-[#132A36] font-semibold mb-2">Cost saving</p>
-                    <p className="text-[#104185] text-xs">Excellent Price & Sales</p>
+            <div className="flex flex-col items-center w-full py-6 px-2">
+                <div className="grid grid-cols-2 py-6 items-center justify-center gap-4">
+                    <div className="flex flex-col border-[1px] rounded-lg border-[#244e65] items-center justify-center text-center h-40 px-6 md:w-[400px] shadow-lg">
+                        <i className="fi fi-rr-shipping-fast text-[#104185] text-3xl"></i>
+                        <p className="text-[#132A36] font-semibold mb-2">Express Delivery</p>
+                        <p className="text-[#104185] text-xs">All over Country</p>
+                    </div>
+                    <div className="flex flex-col border-[1px] rounded-lg border-[#244e65] items-center justify-center text-center h-40 px-6 md:w-[400px] shadow-lg">
+                        <i className="fi fi-rr-feedback-alt text-[#104185] text-3xl"></i>
+                        <p className="text-[#132A36] font-semibold mb-2">Positive Feedback</p>
+                        <p className="text-[#104185] text-xs">99% Customer satisfaction rate</p>
+                    </div>
+                    <div className="flex flex-col border-[1px] rounded-lg border-[#244e65] items-center justify-center text-center h-40 px-6 md:w-[400px] shadow-lg">
+                        <i className="fi fi-rr-restock text-[#104185] text-3xl"></i>
+                        <p className="text-[#132A36] font-semibold mb-2">Easy Return and refunds</p>
+                        <p className="text-[#104185] text-xs">Amazon delivery charges apply</p>
+                    </div>
+                    <div className="flex flex-col border-[1px] rounded-lg border-[#244e65] items-center justify-center text-center h-40 px-6 md:w-[400px] shadow-lg">
+                        <i className="fi fi-rr-chart-mixed-up-circle-dollar text-[#104185] text-3xl"></i>
+                        <p className="text-[#132A36] font-semibold mb-2">Cost saving</p>
+                        <p className="text-[#104185] text-xs">Excellent Price & Sales</p>
+                    </div>
                 </div>
             </div>
 
             {/* Products */}
             <div className="flex flex-col items-center py-6 px-2">
-                <div className="flex flex-row items-center justify-center gap-2 md:gap-0">
+                <div className="flex flex-col md:flex-row items-center justify-center gap-2 md:gap-0">
                     <div className="flex flex-col text-center">
                         <h2 className="text-3xl font-bold text-[#132A36]">Featured Products</h2>
                         <h3 className="text-md text-[#104185]">Our most selling items</h3>
                     </div>
                     <Link to="/products" className="text-xs md:absolute md:right-10 md:text-md md:font-semibold md:justify-end px-4 py-2 border-[1px] border-[#132A36] rounded-lg">All Products</Link>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-2 py-6 w-full">
+                <div className="grid grid-cols-1 md:grid-cols-4 max-w-[400px] md:max-w-full gap-2 py-6 w-full">
                     {products?.map((product) => (
                         <div
                             key={product._id}
