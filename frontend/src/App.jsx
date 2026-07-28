791f061545de26bbc4ebe02be89c82dd98257c0e
Ali Mujahid <mindcracker54321@gmail.com>
2026-07-28 02:19:22 -0400

    Perfomance optimization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import Homepage from "./pages/Hompage";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import VerifyEmail from "./pages/VerifyEmail";
-import Category from "./pages/Category";
+const Homepage = lazy(() => import("./pages/Hompage")) 
+const Register = lazy(() => import("./pages/Register"))
+const Dashboard = lazy(() => import("./pages/Dashboard")) 
+const VerifyEmail = lazy(() => import("./pages/VerifyEmail")) 
+const Category = lazy(() => import("./pages/Category")) 
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import UpdateCategory from "./pages/UpdateCategory";
-import Product from "./pages/Product";
-import AllProducts from "./pages/AllProducts";
-import UpdateProduct from "./pages/UpdateProduct";
-import ProductDetails from "./pages/ProductDetails";
-import Cart from "./pages/Cart";
+const UpdateCategory = lazy(() => import("./pages/UpdateCategory")) 
+const Product = lazy(() => import("./pages/Product"))
+const AllProducts = lazy(() => import("./pages/AllProducts")) 
+const UpdateProduct = lazy(() => import("./pages/UpdateProduct")) 
+const ProductDetails = lazy(() => import("./pages/ProductDetails")) 
+const Cart = lazy(() => import("./pages/Cart")) 
 
 function App() {
     return (
@@ -21,19 +22,21 @@ function App() {
             <Header />
 
             <main className="flex-1">
-                <Routes>
-                    <Route path="/" element={<Homepage />} />
-                    <Route path="/accounts/register" element={<Register />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/verify-email" element={<VerifyEmail />} />
-                    <Route path="/dashboard/category" element={<Category />} />
-                    <Route path="/dashboard/update-category/:slug" element={<UpdateCategory />} />
-                    <Route path="/dashboard/product" element={<Product />} />
-                    <Route path="/products" element={<AllProducts />} />
-                    <Route path="/dashboard/update-product/:slug" element={<UpdateProduct />} />
-                    <Route path="/product/:slug" element={<ProductDetails />} />
-                    <Route path="/cart" element={<Cart />} />
-                </Routes>
+                <Suspense>
+                    <Routes>
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/accounts/register" element={<Register />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/verify-email" element={<VerifyEmail />} />
+                        <Route path="/dashboard/category" element={<Category />} />
+                        <Route path="/dashboard/update-category/:slug" element={<UpdateCategory />} />
+                        <Route path="/dashboard/product" element={<Product />} />
+                        <Route path="/products" element={<AllProducts />} />
+                        <Route path="/dashboard/update-product/:slug" element={<UpdateProduct />} />
+                        <Route path="/product/:slug" element={<ProductDetails />} />
+                        <Route path="/cart" element={<Cart />} />
+                    </Routes>
+                </Suspense>
             </main>
 
             <Footer />
