e9c96f0bf536a9a78a26cf3e4e7f763b0f389fe1
Ali Mujahid <mindcracker54321@gmail.com>
2026-08-31 05:05:34 -0400

    Notifications/Promotional events feature

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -8,10 +8,6 @@ if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET not found in environment variables.");
 }
 
-if (!process.env.PORT) {
-    throw new Error("PORT not found in environment variables.");
-}
-
 if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI not found in environment variables.");
 }
